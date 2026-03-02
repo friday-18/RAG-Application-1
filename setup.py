b3ce5b06e7e6f522a34744be71f8fddd1ef8388f
Harish Marihalkar <friday18.code@gmail.com>
2026-03-03 01:18:11 +0530

    requirements added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="rag_application",
+    version="0.1.0",
+    author="Harish Marihalkar",
+    author_email="friday18.code@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
